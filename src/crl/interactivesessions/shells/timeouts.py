75fb6677092194e9bcf791844e5ce8c409bf5b1a
Huovinen, Petri Juhani <petri.huovinen@nokia.com>
2019-03-15 09:37:16 +0200

    [#6] Make Python short_timeout configurable
    Some times the current default short_timeout in Python shells is too
    short for e.g. waiting prompt. To solve this problem, expose keywords
    for configuring this timeout.

--- src/crl/interactivesessions/shells/timeouts.py
+++ src/crl/interactivesessions/shells/timeouts.py
@@ -1,6 +1,6 @@
 from crl.interactivesessions.shells.shell import DEFAULT_STATUS_TIMEOUT
 from crl.interactivesessions.shells.msgreader import MsgReader
-
+from crl.interactivesessions.shells.pythonshellbase import PythonShellBase
 
 __copyright__ = 'Copyright (C) 2019, Nokia'
 
@@ -8,6 +8,8 @@ __copyright__ = 'Copyright (C) 2019, Nokia'
 class Timeouts(object):
     """Set common timeout for reading login banner and status code.
     """
+
+    _original_short_timeout = 10
 
     @staticmethod
     def set(timeout):
@@ -24,3 +26,14 @@ class Timeouts(object):
         """
         DEFAULT_STATUS_TIMEOUT.reset()
         MsgReader.reset_timeout()
+
+    @staticmethod
+    def set_python_short_timeout(timeout):
+        """Set timeout for short Python shell operations.
+        """
+        PythonShellBase.set_short_timeout(timeout)
+
+    def reset_python_short_timeout(self):
+        """Reset to default the timeout for short Python shell operations.
+        """
+        PythonShellBase.set_short_timeout(self._original_short_timeout)
